3c020f979c33774f7abecbb895064840423b00ac
sadikquake <sadikquake2003@gmail.com>
2023-05-28 19:37:35 +0300

    Added service functions

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-import requests, math
+import requests, math, sys
 from bs4 import BeautifulSoup
+from datetime import date
+
+# message language
+lang = 'en'
+# log file
+log = './logs/' + str(date.today()) + '.log'
 
 # get answer from url
 def get_answer(url = ''):
     url = url.strip()
-    if len(url):
+    if url:
         header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/113.0.0.0 Safari/537.36'}
         try:
-            responce = requests.get(url, headers=header)
-            if len(responce.text):
+            responce = requests.get(url, headers = header)
+            if responce.text:
                 if responce.status_code == 200:
                     return responce.text
                 else:
-                    print('Answer is wrong')
+                    write_file(log, 'Answer is wrong\n')
             else:
-                print('Answer is empty')
+                write_file(log, 'Answer is empty\n')
         except:
-            print('Unable to get a response')
+            write_file(log, 'Unable to get a response\n')
     else:
-        print('Empty url')
+        write_file(log, 'Empty url\n')
 
 # parse of the main page
 def parse_main_page(url):
@@ -28,19 +34,20 @@ def parse_main_page(url):
         soup = BeautifulSoup(answer, 'lxml')
         container = soup.find('div', {'class': 'sirka'})
         sections_containers = container.findAll('span', {'class': 'nadpisnahlavni'})
-        if len(sections_containers):
+        if sections_containers:
             for i in sections_containers:
                 sections.append([i.text, i.find('a').get('href')])
             return sections
         else:
-            print('No sections found')
+            write_file(log, 'No sections found\n')
     except:
-        print('Can\'t parse of the main page')
+        write_file(log, 'Can\'t parse of the main page\n')
 
 # collecting the urls of all pages
 def get_all_url(sections):
     if isinstance(sections, list) and len(sections):
-        urls = []
+        urls = ''
+        urls_file = './data/urls.txt'
         for section in sections:
             try:
                 answer = get_answer(section[1][:-1])
@@ -52,11 +59,46 @@ def get_all_url(sections):
                     count_all_pages = math.ceil(count_ads / count_ads_page)
                     i = 0
                     while i < count_all_pages:
-                        urls.append(section[1] + str(i * count_ads_page) + '/')
+                        if not i:
+                            urls += section[1] + '\n'
+                        else:
+                            urls += section[1] + str(i * count_ads_page) + '/\n'
                         i += 1
                 else:
-                    print('Unable to get information about the section')
+                    write_file(log, 'Unable to get information about the section\n')
             except:
-                print('The response of the requested section was not received')
+                write_file(log, 'The response of the requested section was not received\n')
+        if urls:
+            write_file(urls_file, urls)
     else:
-        print('Sections not found')
+        write_file(log, 'Sections not found\n')
+
+# service functions
+# writing file
+def write_file(file_name = '', data = ''):
+    file_name = file_name.strip()
+    data = data.strip()
+    if file_name and data:
+        try:
+            with open(file_name, 'a') as f:
+                f.write(data)
+                f.close()
+        except:
+            print('Unable to write data to file')
+    else:
+        write_file(log, 'No file name or data to write is specified\n')
+
+# clearing the variable
+def clearning_var(var, type = 'int'):
+    match type:
+        case 'date':
+            new_var = ''.join((x for x in var if x.isdigit() or x == '.'))
+        case 'int' | _:
+            new_var = ''.join((x for x in var if x.isdigit()))
+    return new_var
+
+# checking of requirements
+def check_requirements():
+    if not (sys.version_info.major == 3 and sys.version_info.minor >= 10):
+        print('You need python version 3.10 or higher')
+
